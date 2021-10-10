bcc753c1f9f9406471c8e5b6bd0b24e8bc764b4a
konikasingh <konikasingh1996@gmail.com>
2021-10-10 13:01:03 +0530

    Adding UC1

--- Program.cs
+++ Program.cs
@@ -7,6 +7,18 @@ namespace Employee
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
         }
     }
 }
