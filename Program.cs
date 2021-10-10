0fc3f41aab0519be46ac369b858b65b3853f2b6e
konikasingh <konikasingh1996@gmail.com>
2021-10-10 13:51:28 +0530

    Adding UC2

--- Program.cs
+++ Program.cs
@@ -6,19 +6,24 @@ namespace Employee
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
             int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HOUR = 20;
+
+            int empHrs = 0;
+            int empWage = 0;
             Random random = new Random();
 
             int empCheck = random.Next(0, 2);
             if (empCheck == IS_FULL_TIME)
             {
-                Console.WriteLine("Employee is present");
+                empHrs = 8;
             }
             else
             {
-                Console.WriteLine("Employee is absent");
+                empHrs = 0;
             }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp Wage : " + empWage);
         }
     }
 }
