cf4fb8bda2a2c22a65627e6aa4f02cba581ded07
konikasingh <konikasingh1996@gmail.com>
2021-10-10 15:07:33 +0530

    Final UC6

--- Program.cs
+++ Program.cs
@@ -7,8 +7,8 @@ namespace Employee
         public const int IS_PART_TIME = 1;
         public const int IS_FULL_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
-        public const int NUM_OF_WORKING_DAYS = 2;
-        public const int MAX_HRS_IN_MONTH = 10;
+        public const int NUM_OF_WORKING_DAYS = 20;
+        public const int MAX_HRS_IN_MONTH = 100;
         static void Main(string[] args)
         {
 
