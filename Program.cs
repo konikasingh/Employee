17bf8836393480ea81f4abefc015d16cce3a2be1
konikasingh <konikasingh1996@gmail.com>
2021-10-17 20:39:54 +0530

    Uc9Added Throught Class

--- Program.cs
+++ Program.cs
@@ -2,21 +2,32 @@
 
 namespace Employee
 {
-    class Program
+    public class EmpWageBuilderObject
     {
         public const int IS_PART_TIME = 1;
         public const int IS_FULL_TIME = 2;
-        public const int EMP_RATE_PER_HRS = 20;
-        public const int NUM_OF_WORK_DAYS = 2;
-        public const int MAX_HRS_IN_MONTH = 10;
-        public static void EmpWage(String company, int Emp_Rate_Per_Hrs, int Num_Of_Work_Days, int MaxHrsInMonth)
+
+
+        private string company;
+        private int empRatePerHour;
+        private int numOfWorkingDays;
+        private int maxHoursPerMonth;
+        private int totalEmpWage;
+        public EmpWageBuilderObject(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
         {
-            int empHrs = 0, totalWorkDays = 0;
-            int totalEmpHrs = 0;
-            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkDays < NUM_OF_WORK_DAYS)
+            this.company = company;
+            this.empRatePerHour = empRatePerHour;
+            this.numOfWorkingDays = numOfWorkingDays;
+            this.maxHoursPerMonth = maxHoursPerMonth;
+        }
+        public void computeEmpWage()
+        {
+            int empHrs = 0, totalWorkDays = 0, totalEmpHrs = 0;
+            while (totalEmpHrs <= this.maxHoursPerMonth && totalWorkDays < this.numOfWorkingDays)
             {
+                totalWorkDays++;
                 Random random = new Random();
-                int empCheck = random.Next(0, 3);
+                int empCheck = random.Next(0, 4);
 
                 switch (empCheck)
                 {
@@ -31,17 +42,26 @@ namespace Employee
                         break;
                 }
                 totalEmpHrs += empHrs;
-                Console.WriteLine("Total Employee Hours:" + totalEmpHrs + " "   + "Employee Hours:" + empHrs);
+                Console.WriteLine("Day#:" + totalWorkDays + " Emp Hrs : " + empHrs);
             }
-            int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HRS;
-            Console.WriteLine("Total Employee Wage is:"   + totalEmpWage);
+            totalEmpWage = totalEmpHrs * this.empRatePerHour;
+            Console.WriteLine("Total Employee Wage for company:" + company + " is: " + totalEmpWage);
         }
-
-        public static void Main(String[] args)
+        class Program
         {
-            EmpWage("Google", 1,2,3);
-            EmpWage("Reliance", 12, 50, 9);
-            EmpWage("Mahindra", 15, 5, 25);
+            public static void Main(String[] args)
+            {
+                EmpWageBuilderObject dMart = new EmpWageBuilderObject("DMart", 20, 2, 10);
+                EmpWageBuilderObject Vguard = new EmpWageBuilderObject("Vguard", 10, 2, 10);
+                EmpWageBuilderObject reliance = new EmpWageBuilderObject("Reliancet", 10, 4, 20);
+                dMart.computeEmpWage();
+                Console.WriteLine(dMart.ToString());
+                Vguard.computeEmpWage();
+                Console.WriteLine(Vguard.ToString());
+                reliance.computeEmpWage();
+                Console.WriteLine(reliance.ToString());
+
+            }
         }
 
     }
