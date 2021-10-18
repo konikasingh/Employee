f447468bdc02b147bee41a1819cdb9b74a7dec4c
konikasingh <konikasingh1996@gmail.com>
2021-10-18 14:07:54 +0530

    Added Employee wage for multiple companies

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ namespace Employee
 {
     public class EmpWageBuilderObject
     {
+<<<<<<< HEAD
         public const int IS_PART_TIME = 1;
         public const int IS_FULL_TIME = 2;
 
@@ -62,6 +63,28 @@ namespace Employee
                 Console.WriteLine(reliance.ToString());
 
             }
+=======
+        //public const int EmpRatePerHrs= 20;
+
+        // public const int Num_Of_Working_Days = 20;
+        //public const int WorkingHrs = 60;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation!\n");
+            EmpWageCompute emp = new EmpWageCompute();
+
+            emp.addCompanyEmpWage("DMart", 20, 20, 60);
+            emp.addCompanyEmpWage("Reliance", 20, 15, 30);
+            emp.addCompanyEmpWage("VMware", 10, 20, 30);
+            emp.addCompanyEmpWage("Rolex", 15, 10, 60);
+            emp.ComputeEmpWage();
+
+
+
+
+
+
+>>>>>>> uc10Employee
         }
 
     }
